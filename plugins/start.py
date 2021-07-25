e92addc37be620a39ef725104292cc55e58e0d87
wtmgrup <87792727+wtmgrup@users.noreply.github.com>
2021-07-25 15:07:21 +0800

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -94,11 +94,11 @@ async def start_command(client: Client, message: Message):
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
-    text = "<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>"
+    text = "<b>OUPSS KAMU BUKAN MEMBER BLUE BOX CINEMA\n\nKAMU HARUS SUBSCRIBE CHANNEL BLUE BOX CINEMA DULU JIKA INGIN AKSES MEDIANYA</b>"
     message_text = message.text
     try:
         command, argument = message_text.split()
-        text = text + f" <b>and <a href='https://t.me/{client.username}?start={argument}'>try again</a></b>"
+        text = text + f" <b>and <a href='https://t.me/{client.username}?start={argument}'>COBA LAGI</a></b>"
     except ValueError:
         pass
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Join Channel", url = client.invitelink)]])
